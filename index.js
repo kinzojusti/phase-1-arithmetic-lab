0580403a8f48cb108ff3d705dccfce3b1a06572e
+ <+>
2022-04-04 16:11:31 -0400

    commit tree 25915cea5b28fb7e7c3cf6525567d91a3539df2c

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+//description of variable num1:
+const num1 = 31;
+//description of variable num2:
+const num2 = 2;
+//declaration of variable and solution. 
+const multiply = num2 * num1;
+
+//generate a random integer greater than O
+const random = Math.floor( Math.random() * 1000 ) +1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max( 5, 10, 15, 20 );
